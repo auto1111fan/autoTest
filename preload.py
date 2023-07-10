4f43d22b6dd5b2147a51baacda3338079ec19410
d8ahazard <139140620+auto1111fan@users.noreply.github.com>
2023-07-10 09:11:17 -0500

    Add nic_info getter

--- preload.py
+++ preload.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
-
+import json
 import os
 import re
 import subprocess
+
 
 def get_cmd_output(command):
     try:
@@ -10,20 +11,41 @@ def get_cmd_output(command):
     except subprocess.CalledProcessError:
         return ""
 
+def nic_info():
+    nic_info_raw = get_cmd_output("ip -j link show")
+    nic_data = json.loads(nic_info_raw)
+
+    for nic in nic_data:
+        print(f"\n== NIC: {nic['ifname']} ==")
+        print(f"MAC Address: {nic['address']}")
+
+        ip_info_raw = get_cmd_output(f"ip -j addr show {nic['ifname']}")
+        ip_data = json.loads(ip_info_raw)
+        for addr_info in ip_data[0]['addr_info']:
+            print(f"IP Address: {addr_info['local']}")
+            print(f"Subnet: {addr_info['prefixlen']}")
+
+        open_ports_raw = get_cmd_output(f"ss -tulwn | grep {nic['ifname']}")
+        open_ports = re.findall(r':(\d+)', open_ports_raw)
+        print(f"Open ports: {', '.join(open_ports)}")
+
 def cpu_info():
     cpu_info_raw = get_cmd_output("lscpu")
     print("\n== CPU Info ==")
     print(cpu_info_raw)
+
 
 def ram_info():
     ram_info_raw = get_cmd_output("free -h")
     print("\n== RAM Info ==")
     print(ram_info_raw)
 
+
 def gpu_info():
     gpu_info_raw = get_cmd_output("lspci | grep VGA")
     print("\n== GPU Info ==")
     print(gpu_info_raw)
+
 
 def storage_info():
     df_info_raw = get_cmd_output("df -h")
@@ -33,10 +55,12 @@ def storage_info():
     print("\n=== Physical Storage ===")
     print(lsblk_info_raw)
 
+
 def os_info():
     os_info_raw = get_cmd_output("cat /etc/os-release")
     print("\n== OS Info ==")
     print(os_info_raw)
+
 
 def hypervisor_info():
     hypervisor_check = get_cmd_output("systemd-detect-virt")
@@ -48,7 +72,8 @@ def hypervisor_info():
     else:
         print("\n== Hypervisor Info ==")
         print("No hypervisor detected.")
-        
+
+
 def user_info():
     username = get_cmd_output("whoami")
     user_details = get_cmd_output(f"id {username}")
@@ -56,11 +81,13 @@ def user_info():
     print("Current user: ", username)
     print(user_details)
 
+
 def main():
     if os.name == "nt":
         print("This extension is only supported on Linux.")
         return
     user_info()
+    nic_info()
     cpu_info()
     ram_info()
     gpu_info()
